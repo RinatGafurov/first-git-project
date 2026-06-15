ef024315ef4cba391a54538561a32407eac565d4
Ринат Гафуров <rgafurov@unislabs.com>
2026-06-15 12:11:44 +0500

    Add sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -5,5 +5,6 @@ public class FirstGit {
         System.out.println("Git learning");
         System.out.println("is dif");
         System.out.println("A'm happy");
+        System.out.println("!!!!!");
     }
 }
