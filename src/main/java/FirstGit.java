60ce44e74cd7d3bb930a91546670d35ff03ffae6
RinatGafurov <95069493+RinatGafurov@users.noreply.github.com>
2026-06-15 11:38:47 +0500

    Update FirstGit.java
    Add ! to sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -1,7 +1,7 @@
 public class FirstGit {
 
     public static void main(String[] args) {
-        System.out.println("Git");
+        System.out.println("Git!");
         System.out.println("Git learning");
     }
 }
