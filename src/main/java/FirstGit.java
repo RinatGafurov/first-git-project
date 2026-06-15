f966d11f4582977524edcc0a4ba0ec234ce8ac09
Ринат Гафуров <rgafurov@unislabs.com>
2026-06-15 12:10:42 +0500

    Add developer

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -1,7 +1,7 @@
 public class FirstGit {
 
     public static void main(String[] args) {
-        System.out.println("Git!");
+        System.out.println("GIT DEVELOPER");
         System.out.println("Git learning");
         System.out.println("is dif");
         System.out.println("A'm happy");
