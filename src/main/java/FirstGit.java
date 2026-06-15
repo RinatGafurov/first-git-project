cdda43d4077b32f37ed3f53071aaab9a547777e6
Ринат Гафуров <rgafurov@unislabs.com>
2026-06-15 12:14:11 +0500

    delete sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -3,7 +3,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT DEVELOPER");
         System.out.println("Git learning");
-        System.out.println("is dif");
         System.out.println("A'm happy");
         System.out.println("!!!!!");
     }
