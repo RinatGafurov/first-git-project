179789eb7556d81a46f2630d740d3680d3e9774f
Ринат Гафуров <rgafurov@unislabs.com>
2026-06-15 11:42:14 +0500

    Add sout is diff

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -3,5 +3,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Git!");
         System.out.println("Git learning");
+        System.out.println("is dif");
     }
 }
